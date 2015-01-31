bc38716177c025ba08a985f30b82d78fd43f0850
bplittle <bplittle@gmail.com>
2015-01-31 20:23:02 +0000

    captures all followers' payments. Basic formatting. Fixed some routes

--- app/actions.rb
+++ app/actions.rb
@@ -31,7 +31,7 @@ post '/leader/new' do
 
     leader = Leader.new(
         leader_name: params[:leader_name],
-        leader_email: parmas[:leader_email],
+        leader_email: params[:leader_email],
         password: params[:password],
         leader_stripe_key: params[:api_key])
 
@@ -62,7 +62,7 @@ post '/new_user' do
 end
 
 get '/events' do
-    erb :index
+    erb :events
 end
 
 get '/events/new' do
@@ -88,7 +88,7 @@ post '/events/new' do
         follower.save
     end
 
-    redirect '/'
+    redirect '/event'
 
     # binding.pry
     
@@ -103,19 +103,6 @@ get '/events/:event_id' do
     erb :'events/show'
 end
 
-post '/events/:event_id' do
- 
-## take payment from folloewrs
-	event = Event.find(params[:id])
-
-	event.followers.each do |follower|
-		follower[:stripe_token].capture
-	end
-
-	redirect '/events'
-
-end
-
 post '/events/:event_id/decline/:follower_id' do
 
 	follower = Follower.where(event_id: params[:event_id], id: params[:follower_id]).first
@@ -127,61 +114,78 @@ post '/events/:event_id/decline/:follower_id' do
 end
 
 post '/events/:event_id/:follower_id' do
-# binding.pry
-follower = Follower.where(event_id: params[:event_id], id: params[:follower_id]).first
-follower[:follower_name] = params[:name].to_s
-follower[:unit_quantity] = params[:unit_quantity].to_i
-follower[:unit_total_price] = follower[:unit_quantity].to_i * Event.find(params[:event_id]).unit_price
-follower[:status] = params[:status]
+    # binding.pry
+    follower = Follower.where(event_id: params[:event_id], id: params[:follower_id]).first
+    follower[:follower_name] = params[:name].to_s
+    follower[:unit_quantity] = params[:unit_quantity].to_i
+    follower[:unit_total_price] = follower[:unit_quantity].to_i * Event.find(params[:event_id]).unit_price
+    follower[:status] = params[:status]
 
- p follower[:follower_name]
+     p follower[:follower_name]
 
-       @amount = 2000
+           @amount = 2000
 
- Stripe.api_key = "sk_test_i4TQCuPANmpmhYs6I9YXgV4m"
+     Stripe.api_key = "sk_test_i4TQCuPANmpmhYs6I9YXgV4m"
 
- puts "strike api key received"
+     puts "strike api key received"
 
-# Get the credit card details submitted by the form
-token = params[:stripeToken]
-p token
+    # Get the credit card details submitted by the form
+    token = params[:stripeToken]
+    p token
 
-# Create the charge on Stripe's servers - this will charge the user's card
+    # Create the charge on Stripe's servers - this will charge the user's card
 
-  charge = Stripe::Charge.create(
-    :amount => 1000, # amount in cents, again
-    :currency => "cad",
-    :card => token,
-    :description => "payinguser@example.com",
-    :capture => false
-  )
-# rescue Stripe::CardError => e
-#   # The card has been declined
-
+      charge = Stripe::Charge.create(
+        :amount => 1000, # amount in cents, again
+        :currency => "cad",
+        :card => token,
+        :description => "payinguser@example.com",
+        :capture => false
+      )
+    # rescue Stripe::CardError => e
+    #   # The card has been declined
 
 
  	follower[:stripe_token] = charge.id
- 	if follower.save
- 		follower[:status] = "confirmed"
- 		puts "success"
- 	end
+	follower[:status] = "confirmed"
+	puts "success"
+ 	
 
   redirect '/events'
      
 end
 
-post '/events' do
-	#add id back in later to specify event
+post '/events/:event_id' do
+	event = Event.find(params[:event_id])
 
 	Stripe.api_key = "sk_test_i4TQCuPANmpmhYs6I9YXgV4m"
 
-	ch = Stripe::Charge.retrieve("ch_15QoUXFR7CapK6EtvYMQZC2P")
-	binding.pry
-	ch.capture
+    event.followers.each do |follower|
+        charge_token = follower[:stripe_token]
+        ch = Stripe::Charge.retrieve(charge_token)
+        ch.capture
+    end
+
+	# ch = Stripe::Charge.retrieve("ch_15QoUXFR7CapK6EtvYMQZC2P")
+	# binding.pry
+	# ch.capture
 
 	redirect '/'
 
 end
+
+# post '/events/:event_id' do
+ 
+# ## take payment from folloewrs
+#     event = Event.find(params[:id])
+
+#     event.followers.each do |follower|
+#         follower[:stripe_token].capture
+#     end
+
+#     redirect '/events'
+
+# end
 
 get '/events/:event_id/:follower_id' do
     @unit_price = Event.find(params[:event_id]).unit_price
